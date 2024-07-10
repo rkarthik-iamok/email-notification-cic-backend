1c1e22575d3396d6b2e2dfea7a70543bf5ca8b3b
rkarthik-iamok <rkarthik.iamok@gmail.com>
2024-07-10 09:24:51 -0400

    Added JWT checks

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,6 @@
 const express = require("express");
+const { expressjwt: jwt } = require("express-jwt");
+const jwks = require("jwks-rsa");
 require("dotenv").config;
 
 // Import the Environment Variables
@@ -7,6 +9,20 @@ const PORT = process.env.PORT || "8000";
 // Create the Express Listener App
 const app = express();
 
+// JWT Check
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    ratelimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri:
+      "https://indigo-lobster-5315.cic-demo-platform.auth0app.com/.well-known/jwks.json",
+  }),
+  audience: "https://email-notification-backend.vercel.app/",
+  issuer: "https://indigo-lobster-5315.cic-demo-platform.auth0app.com/",
+  algorithms: ["RS256"],
+});
+
 // Routes
 app.get("/", (req, res) => {
   res.status(200);
@@ -14,6 +30,28 @@ app.get("/", (req, res) => {
     SERVICE: "Email Notification Backend",
     SERVICE_STATUS: "Running",
   });
+});
+
+// Protected Routes
+app.get("/private", jwtCheck, (req, res) => {
+  res.json({
+    service: "Email Notification Backend Service",
+    version: "1.0",
+    author: "KR",
+    api: {
+      type: "private",
+    },
+  });
+});
+
+app.use(function (err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({
+      status: "Invalid Token",
+    });
+  } else {
+    next(err);
+  }
 });
 
 // Start listening
